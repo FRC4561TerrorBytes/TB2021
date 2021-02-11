0ffaf25d3cc98d5ad28828bccf8890fcc6e5a523
Justin Merrill <jmerrill23@rthighschool.org>
2021-02-11 18:50:45 -0500

    Added Continuous Autos for Red+Blue for Paths A+B

---
+++ src/main/java/frc/robot/commands/automodes/PathABlueContinuous.java
@@ -0,0 +1,21 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.automodes;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.AutoTrajectory;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class PathABlueContinuous extends SequentialCommandGroup {
+  /** Creates a new PathA_BlueContinuous. */
+  public PathABlueContinuous(DriveSubsystem driveSubsystem) {
+   super(
+    new AutoTrajectory(driveSubsystem, "output/PathA_BlueAll.wpilib.json").getCommand()
+   );
+  }
+}

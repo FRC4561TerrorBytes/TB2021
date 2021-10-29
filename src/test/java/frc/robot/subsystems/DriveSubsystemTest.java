11cfee415f1647a5cf2feebb136485723af05677
Vignesh Balasubramaniam <vigneshb.hp@gmail.com>
2021-10-29 19:27:56 -0400

    Add comments

--- src/test/java/frc/robot/subsystems/DriveSubsystemTest.java
+++ src/test/java/frc/robot/subsystems/DriveSubsystemTest.java
@@ -29,6 +29,8 @@ public class DriveSubsystemTest {
   @Before
   public void setup() {
     assert HAL.initialize(500, 0);
+
+    // Create mock hardware devices
     m_leftMasterMotor = mock(WPI_TalonFX.class);
     m_rightMasterMotor = mock(WPI_TalonFX.class);
     m_leftSlaveMotor = mock(WPI_TalonFX.class);
@@ -36,10 +38,13 @@ public class DriveSubsystemTest {
     m_lidar = mock(Counter.class);
     m_navx = mock(AHRS.class);
 
+    // Create mock DifferentialDrive object
     m_drivetrain = mock(DifferentialDrive.class);
 
+    // Create Hardware object using mock objects
     m_drivetrainHardware = new DriveSubsystem.Hardware(m_drivetrain, m_leftMasterMotor, m_rightMasterMotor, m_leftSlaveMotor, m_rightSlaveMotor, m_lidar, m_navx);
 
+    // Create DriveSubsystem object
     m_driveSubsystem = new DriveSubsystem(m_drivetrainHardware, 
                                           Constants.DRIVE_kP,
                                           Constants.DRIVE_kD, 
@@ -49,50 +54,79 @@ public class DriveSubsystemTest {
                                           Constants.DRIVE_TRACTION_CONTROL_CURVE);
   }
 
+  // Close out hardware to prevent one test from causing subsequent tests to fail
   @After
   public void close() {
     m_driveSubsystem.close();
     m_driveSubsystem = null;
   }
 
+  /**
+   * Test if robot can move forward using PID drive
+   */
   @Test
   public void goingForward() {
+    // Hardcode NAVX sensor return value for angle, velocityX, and velocityY
     when(m_navx.getAngle()).thenReturn(0.0);
     when(m_navx.getVelocityX()).thenReturn((float)3.966);
     when(m_navx.getVelocityY()).thenReturn((float)0.0);
 
+    // Fill up velocity moving average buffer by calling periodic
     for (int i = 0; i < 60; i++) { m_driveSubsystem.periodic(); }
+
+    // Try to drive forward
     m_driveSubsystem.teleopPID(1.0, 0.0, 1);
 
+    // Verify if arcadeDrive method was called with expected values
     verify(m_drivetrain, times(1)).arcadeDrive(AdditionalMatchers.eq(1.0, DELTA), AdditionalMatchers.eq(0.0, 0.0), eq(false));
   }
 
+  /**
+   * Test if robot can turn left using PID drive
+   */
   @Test
   public void turningLeft() {
+    // Hardcode NAVX sensor return value for angle
     when(m_navx.getAngle()).thenReturn(0.0);
 
+    // Try to turn left
     m_driveSubsystem.teleopPID(0.0, -1.0, 1);
 
+    // Verify if arcadeDrive method was called with expected values
     verify(m_drivetrain, times(1)).arcadeDrive(AdditionalMatchers.eq(0.0, 0.0), AdditionalMatchers.gt(0.0), eq(false));
   }
 
+  /**
+   * Test if robot can turn right using PID drive
+   */
   @Test
   public void turningRight() {
+    // Hardcode NAVX sensor return value for angle
     when(m_navx.getAngle()).thenReturn(0.0);
 
+    // Try to turn right
     m_driveSubsystem.teleopPID(0.0, 1.0, 1);
 
+    // Verify if arcadeDrive was called with expected values
     verify(m_drivetrain, times(1)).arcadeDrive(AdditionalMatchers.eq(0.0, 0.0), AdditionalMatchers.lt(0.0), eq(false));
   }
 
+  /**
+   * Test if robot will limit wheel slip
+   */
   @Test
   public void tractionControl() {
+    // Hardcode NAVX sensor values for velocityX, velocityY
     when(m_navx.getVelocityX()).thenReturn((float)0.0);
     when(m_navx.getVelocityY()).thenReturn((float)0.0);
 
+    // Fill up velocity moving average buffer by calling periodic
     for (int i = 0; i < 60; i++) { m_driveSubsystem.periodic(); }
+
+    // Try to move forward
     m_driveSubsystem.teleopPID(1.0, 0.0, 1);
 
+    // Verify if arcadeDrive was called with expected values
     verify(m_drivetrain, times(1)).arcadeDrive(AdditionalMatchers.eq(0.03, DELTA), AdditionalMatchers.eq(0.0, 0.0), eq(false));
   }
 }
